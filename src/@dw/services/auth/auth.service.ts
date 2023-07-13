6dc784ce15a09d9cd20c111b4ae93bf094b3ac07
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-07-13 18:50:55 +0900

    auth service getEcodetest

--- src/@dw/services/auth/auth.service.ts
+++ src/@dw/services/auth/auth.service.ts
@@ -7,73 +7,77 @@ import { shareReplay, tap } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 interface Token {
-  token: String
+    token: String
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-	constructor(
-		private http: HttpClient,
-		private jwtHelper: JwtHelperService,
+    constructor(
+        private http: HttpClient,
+        private jwtHelper: JwtHelperService,
 
-	) { }
+    ) { }
 
-	signUp(userData) {
-		return this.http.post('/api/v1/auth/signUp', userData);
-	}
+    signUp(userData) {
+        return this.http.post('/api/v1/auth/signUp', userData);
+    }
 
-	signIn(userData): Observable<Token> {
-		return this.http.post<Token>('/api/v1/auth/signIn', userData)
-		.pipe(
-			shareReplay(),
-			tap( 
-			(res:any) => {
-						this.setToken(res.token)
-					}),
-					shareReplay()
-		)
-	}
+    signIn(userData): Observable<Token> {
+        return this.http.post<Token>('/api/v1/auth/signIn', userData)
+            .pipe(
+                shareReplay(),
+                tap(
+                    (res: any) => {
+                        this.setToken(res.token)
+                    }),
+                shareReplay()
+            )
+    }
 
-	// get verification code + email
-	getEcode(emailData) {
-		return this.http.post('/api/v1/auth/getEcode', emailData)
-	}
+    // get verification code + email
+    getEcode(emailData) {
+        return this.http.post('/api/v1/auth/getEcode', emailData)
+    }
 
-	// set temp password + email
-	getTempPw(emailData) {
-		return this.http.put('/api/v1/auth/getTempPw', emailData)
-	}
+    getEcodetest(emailData) {
+        return this.http.post('/api/v1/auth/getEcodetest', emailData)
+    }
 
-  	logOut(): void {
-		this.removeToken();
-	}
+    // set temp password + email
+    getTempPw(emailData) {
+        return this.http.put('/api/v1/auth/getTempPw', emailData)
+    }
 
-	isAuthenticated(): boolean {
-		const token = this.getToken();
-		return token ? !this.isTokenExpired(token) : false;
-	}
+    logOut(): void {
+        this.removeToken();
+    }
 
-  	getToken(): string {
-		return localStorage.getItem(ENV.tokenName);
-	}
+    isAuthenticated(): boolean {
+        const token = this.getToken();
+        return token ? !this.isTokenExpired(token) : false;
+    }
 
-	setToken(token: string): void {
-		localStorage.setItem(ENV.tokenName, token);
-	}
+    getToken(): string {
+        return localStorage.getItem(ENV.tokenName);
+    }
 
-	removeToken(): void {
-		localStorage.removeItem(ENV.tokenName);
-	}
+    setToken(token: string): void {
+        localStorage.setItem(ENV.tokenName, token);
+    }
 
-	// jwtHelper
-	isTokenExpired(token: string) {
-		return this.jwtHelper.isTokenExpired(token);
-	}
+    removeToken(): void {
+        localStorage.removeItem(ENV.tokenName);
+    }
 
-	getTokenInfo() {
-		return this.jwtHelper.decodeToken(this.getToken());
-	}
+    // jwtHelper
+    isTokenExpired(token: string) {
+        return this.jwtHelper.isTokenExpired(token);
+    }
+
+    getTokenInfo() {
+        return this.jwtHelper.decodeToken(this.getToken());
+    }
 }
