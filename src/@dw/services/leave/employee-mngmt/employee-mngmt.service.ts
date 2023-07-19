fc82cf88eb8f676df5221528639b53da36942c31
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-07-19 18:25:15 +0900

    edit employee info get API setvice test

--- src/@dw/services/leave/employee-mngmt/employee-mngmt.service.ts
+++ src/@dw/services/leave/employee-mngmt/employee-mngmt.service.ts
@@ -2,45 +2,49 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root'
 })
 export class EmployeeMngmtService {
-	constructor(
-		private http: HttpClient,
-	) { }
+    constructor(
+        private http: HttpClient,
+    ) { }
 
-	getPending() {
-		return this.http.get('/api/v1/leave/pending-list');
-	}
+    getPending() {
+        return this.http.get('/api/v1/leave/pending-list');
+    }
 
-	acceptRequest(sendData) {
-		return this.http.put('/api/v1/leave/accept-request', sendData);
-	}
+    acceptRequest(sendData) {
+        return this.http.put('/api/v1/leave/accept-request', sendData);
+    }
 
-	cancelRequest(id) {
-		return this.http.delete('/api/v1/leave/cancel-request/' + id);
-	}
+    cancelRequest(id) {
+        return this.http.delete('/api/v1/leave/cancel-request/' + id);
+    }
 
-	// 매니저가 관리 중인 직원들 리스트
-	getMyEmployeeList() {
-		return this.http.get('/api/v1/leave/myEmployee-list');
-	}
+    // 매니저가 관리 중인 직원들 리스트
+    getMyEmployeeList() {
+        return this.http.get('/api/v1/leave/myEmployee-list');
+    }
 
-	getEmployeeInfo(id) {
-		return this.http.get('/api/v1/leave/employee-info/' + id);
-	}
+    getEmployeeInfo(id) {
+        return this.http.get('/api/v1/leave/employee-info/' + id);
+    }
 
-	putEmployeeInfo(sendData) {
-		return this.http.put('/api/v1/leave/put-employee-info', sendData);
-	}
+    getEmployeeInfotest(id) {
+        return this.http.get('/api/v1/leave/employee-infotest/' + id);
+    }
 
-	// 매니저가 관리중인 직원 휴가 리스트
-	getMyEmployeeLeaveListSearch(data){
-		return this.http.get('/api/v1/leave/myEmployee-leaveList-search',{ params: data });
-	}
+    putEmployeeInfo(sendData) {
+        return this.http.put('/api/v1/leave/put-employee-info', sendData);
+    }
 
-	// admin 이 관리하는 manager의 employee 리스트 가져오기
-	getMyManagerEmployeeList(managerID){
-		return this.http.get('/api/v1/leave/myManager-employee-list',{ params: managerID });
-	}
+    // 매니저가 관리중인 직원 휴가 리스트
+    getMyEmployeeLeaveListSearch(data) {
+        return this.http.get('/api/v1/leave/myEmployee-leaveList-search', { params: data });
+    }
+
+    // admin 이 관리하는 manager의 employee 리스트 가져오기
+    getMyManagerEmployeeList(managerID) {
+        return this.http.get('/api/v1/leave/myManager-employee-list', { params: managerID });
+    }
 }
