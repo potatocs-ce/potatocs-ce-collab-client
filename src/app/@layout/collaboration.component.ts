241695d46a594f8e50d4051326822b84e7a117c8
limhokyun <lim@nsmarts.co.kr>
2023-08-28 13:13:31 +0900

    에러 발생으로 주석처리

--- src/app/@layout/collaboration.component.ts
+++ src/app/@layout/collaboration.component.ts
@@ -131,17 +131,17 @@ export class CollaborationComponent implements OnInit, OnDestroy {
 
     // 처음에는 router event는 안들어옴 (component 생성전?) --> startWith를 이용. url check 가능한 듯.
     // Initial Router Test
-    https://stackoverflow.com/questions/43237318/angular-2-router-event-not-firing-first-time
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        startWith(this.router)
-      )
-      .subscribe((event: NavigationEnd) => {
-        // there will be first router.url - and next - router events
-        console.log(event.url); // .RouterState.snapshot.url
+    // https://stackoverflow.com/questions/43237318/angular-2-router-event-not-firing-first-time
+    // this.router.events
+    //   .pipe(
+    //     filter((event) => event instanceof NavigationEnd),
+    //     startWith(this.router)
+    //   )
+    //   .subscribe((event: NavigationEnd) => {
+    //     // there will be first router.url - and next - router events
+    //     console.log(event.url); // .RouterState.snapshot.url
 
-      });
+    //   });
 
   }
 
