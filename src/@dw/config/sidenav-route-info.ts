9dc1cd7ad95beb089e77410684851904af51d4c7
park9379 <109051896+park9379@users.noreply.github.com>
2023-08-30 16:10:48 +0900

    Added a new subheading under project with options for creating a space and a dropdown for managing spaces. The dropdown currently has no children.

--- src/@dw/config/sidenav-route-info.ts
+++ src/@dw/config/sidenav-route-info.ts
@@ -34,6 +34,30 @@ export const sidenavRouteInfo: NavigationItem[] = [
 		]	
 	},
 
+	// zion
+		// study
+		{
+			type: 'subheading',
+			label: 'study',
+			children: [
+				{
+					type: 'click',
+					label: 'Create space',
+					icon: 'create_new_folder'
+				},
+				{
+					type: 'dropdown',
+					label: 'Space',
+					icon: 'library_books',
+					isManager: false,
+					children: [
+					
+					]
+				}
+			]	
+		},
+	// end
+
 
 	// Leave
 	{
