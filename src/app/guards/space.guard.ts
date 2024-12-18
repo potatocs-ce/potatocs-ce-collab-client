f6e21ab47bde4c430701f78f31cc340b633b20cf
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 14:39:07 +0900

    안쓰는 코드 삭제

--- src/app/guards/space.guard.ts
+++ src/app/guards/space.guard.ts
@@ -1,34 +1,44 @@
-import { inject } from "@angular/core";
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateFn } from "@angular/router";
-import { DialogService } from "../stores/dialog/dialog.service";
-import { AuthService } from "../services/auth/auth.service";
-import { SideNavService } from "../stores/side-nav/side-nav.service";
+import { inject } from '@angular/core';
+import {
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateFn,
+} from '@angular/router';
+import { DialogService } from '../stores/dialog/dialog.service';
+import { AuthService } from '../services/auth/auth.service';
+import { SideNavService } from '../stores/side-nav/side-nav.service';
 
-export const SpaceGuard: CanActivateFn = async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-    const dialogService = inject(DialogService);
-    const router = inject(Router);
-    const sidenavService = inject(SideNavService);
-    const spaces: any = await sidenavService.updateSideMenu().toPromise();
-    const spaceTime = route.params['spaceTime'];
+export const SpaceGuard: CanActivateFn = async (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const dialogService = inject(DialogService);
+  const router = inject(Router);
+  const sidenavService = inject(SideNavService);
+  const spaces: any = await sidenavService.updateSideMenu().toPromise();
+  const spaceTime = route.params['spaceTime'];
 
-    let spaceInfo = spaces.navList[0].spaces;
-    let flag = false
+  let spaceInfo = spaces.navList[0].spaces;
+  let flag = false;
 
-    for (let index = 0; index < spaceInfo.length; index++) {
-        const element = spaceInfo[index]._id;
-        if (spaceTime == element) {
-            flag = true;
-            break;
-        } else {
-            flag = false;
-        }
-    }
-    console.log(flag)
-    if (flag) {
-        return true
+  for (let index = 0; index < spaceInfo.length; index++) {
+    const element = spaceInfo[index]._id;
+    if (spaceTime == element) {
+      flag = true;
+      break;
     } else {
-        dialogService.openDialogNegative("You are not a member of this space or document.");
-        router.navigate(["/main"]);
+      flag = false;
     }
-    return false
-}
+  }
+
+  if (flag) {
+    return true;
+  } else {
+    dialogService.openDialogNegative(
+      'You are not a member of this space or document.'
+    );
+    router.navigate(['/main']);
+  }
+  return false;
+};
