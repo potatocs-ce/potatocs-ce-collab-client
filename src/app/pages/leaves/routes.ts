640cdb29ee227aab7f6e55f54e6cafc77d25a1a8
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 11:19:07 +0900

    status 삭제

--- src/app/pages/leaves/routes.ts
+++ src/app/pages/leaves/routes.ts
@@ -1,13 +1,4 @@
 import { Route } from '@angular/router';
-import { StatusComponent } from './status/status.component';
 import { LeavesComponent } from './leaves.component';
 
-export const LEAVES_ROUTES: Route[] = [
-  {
-    // 이정운 작업
-    // path: 'status', // 휴가 사용 현황
-    // loadComponent: () => StatusComponent
-    path: 'my-status',
-    loadComponent: () => StatusComponent,
-  },
-];
+export const LEAVES_ROUTES: Route[] = [];
