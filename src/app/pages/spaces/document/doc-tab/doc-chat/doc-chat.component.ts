761b864e7fd2c874d74e8aeb2ddc42da1600f11b
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 14:48:45 +0900

    안쓰는 코드 삭제

--- src/app/pages/spaces/document/doc-tab/doc-chat/doc-chat.component.ts
+++ src/app/pages/spaces/document/doc-tab/doc-chat/doc-chat.component.ts
@@ -1,174 +1,170 @@
-import { AfterViewInit, Component, OnInit } from "@angular/core";
-import { DocumentsService } from "../../../../../services/spaces/documents.service";
-import { Router, ActivatedRoute } from "@angular/router";
-import { CommonService } from "../../../../../services/common/common.service";
-import { ProfilesService } from "../../../../../services/profiles/profiles.service";
-import { ViewChild, ElementRef } from "@angular/core";
-import { ThemePalette } from "@angular/material/core";
-import { DialogService } from "../../../../../stores/dialog/dialog.service";
-import { MaterialsModule } from "../../../../../materials/materials.module";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { DocumentsService } from '../../../../../services/spaces/documents.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CommonService } from '../../../../../services/common/common.service';
+import { ProfilesService } from '../../../../../services/profiles/profiles.service';
+import { ViewChild, ElementRef } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { DialogService } from '../../../../../stores/dialog/dialog.service';
+import { MaterialsModule } from '../../../../../materials/materials.module';
 @Component({
-	selector: "app-doc-chat",
-	templateUrl: "./doc-chat.component.html",
-	styleUrls: ["./doc-chat.component.scss"],
-	standalone: true,
-	imports: [MaterialsModule],
+  selector: 'app-doc-chat',
+  templateUrl: './doc-chat.component.html',
+  styleUrls: ['./doc-chat.component.scss'],
+  standalone: true,
+  imports: [MaterialsModule],
 })
 export class DocChatComponent implements OnInit, AfterViewInit {
-	public chatContent;
-	public chatDoc = [];
-	public paramObject;
-	public docId;
-	public replyFlag = false;
-	public replyContent;
-	public replyMember;
-	public replyChatId;
-	public userId;
+  public chatContent;
+  public chatDoc = [];
+  public paramObject;
+  public docId;
+  public replyFlag = false;
+  public replyContent;
+  public replyMember;
+  public replyChatId;
+  public userId;
 
-	mobileWidth: any;
+  mobileWidth: any;
 
-	@ViewChild("target") private myScrollContainer: ElementRef;
-	scrolltop: number = null;
+  @ViewChild('target') private myScrollContainer: ElementRef;
+  scrolltop: number = null;
 
-	constructor(
-		private docService: DocumentsService,
-		// private router: Router,
-		private route: ActivatedRoute,
-		private commonService: CommonService,
-		private profileService: ProfilesService,
-		private dialogService: DialogService
-	) {}
+  constructor(
+    private docService: DocumentsService,
+    // private router: Router,
+    private route: ActivatedRoute,
+    private commonService: CommonService,
+    private profileService: ProfilesService,
+    private dialogService: DialogService
+  ) {}
 
-	ngOnInit(): void {
-		// 유저 아이디 가져오기
-		this.profileService.getUserProfile().subscribe((data: any) => {
-			// console.log(data);
-			this.userId = data.user._id;
-		});
-		// console.log(this.route.queryParamMap);
+  ngOnInit(): void {
+    // 유저 아이디 가져오기
+    this.profileService.getUserProfile().subscribe((data: any) => {
+      this.userId = data.user._id;
+    });
 
-		// doc id 가져오기
-		this.route.queryParamMap.subscribe((params: any) => {
-			this.paramObject = params.params;
-			this.docId = this.paramObject.id;
-		});
-		this.getChatInDoc();
-		this.scrollToBottom();
-	}
+    // doc id 가져오기
+    this.route.queryParamMap.subscribe((params: any) => {
+      this.paramObject = params.params;
+      this.docId = this.paramObject.id;
+    });
+    this.getChatInDoc();
+    this.scrollToBottom();
+  }
 
-	ngAfterViewInit() {
-		this.browserCheck();
-	}
+  ngAfterViewInit() {
+    this.browserCheck();
+  }
 
-	createChat() {
-		// this.chatDoc.push(this.chatContent);
-		var data = {
-			docId: this.docId,
-			chatContent: this.chatContent,
-			isDialog: true,
-		};
-		if (this.replyFlag) {
-			// console.log(this.replyChatId);
-			data["replyChatId"] = this.replyChatId;
-		}
-		// console.log(data);
+  createChat() {
+    // this.chatDoc.push(this.chatContent);
+    var data = {
+      docId: this.docId,
+      chatContent: this.chatContent,
+      isDialog: true,
+    };
+    if (this.replyFlag) {
+      data['replyChatId'] = this.replyChatId;
+    }
 
-		this.docService.createChat(data).subscribe({
-			next: (data: any) => {
-				// console.log(data);
-				this.getChatInDoc();
-				this.replyCancel();
-				this.chatContent = "";
-			},
-			error: (err: any) => {
-				console.log(err);
-			},
-		});
-	}
-	deleteChat(chatId, replyId) {
-		// const result = confirm('채팅을 삭제하시겠습니끼?');
-		// if(result){
+    this.docService.createChat(data).subscribe({
+      next: (data: any) => {
+        this.getChatInDoc();
+        this.replyCancel();
+        this.chatContent = '';
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
+  deleteChat(chatId, replyId) {
+    // const result = confirm('채팅을 삭제하시겠습니끼?');
+    // if(result){
 
-		this.dialogService.openDialogConfirm("Do you want to delete this chat?").subscribe((result) => {
-			if (result) {
-				const data = {
-					chatId: chatId,
-					replyId: replyId,
-				};
-				console.log(data);
-				this.docService.deleteChat(data).subscribe(
-					(data: any) => {
-						// console.log(data);
-						this.getChatInDoc();
-					},
-					(err: any) => {
-						console.log(err);
-					}
-				);
-			}
-		});
-	}
+    this.dialogService
+      .openDialogConfirm('Do you want to delete this chat?')
+      .subscribe((result) => {
+        if (result) {
+          const data = {
+            chatId: chatId,
+            replyId: replyId,
+          };
 
-	getChatInDoc() {
-		const data = {
-			docId: this.docId,
-			from: "document",
-		};
-		this.docService.getChatInDoc(data).subscribe(
-			(data: any) => {
-				// console.log(data);
-				this.chatDoc = data.getChatInDoc;
+          this.docService.deleteChat(data).subscribe(
+            (data: any) => {
+              this.getChatInDoc();
+            },
+            (err: any) => {
+              console.log(err);
+            }
+          );
+        }
+      });
+  }
 
-				for (let index = 0; index < this.chatDoc.length; index++) {
-					const date = this.commonService.dateFormatting(this.chatDoc[index].createdAt, "chatDate");
-					this.chatDoc[index].createdAt = date;
-				}
-				console.log(this.chatDoc);
-			},
-			(err: any) => {
-				console.log(err);
-			}
-		);
-	}
+  getChatInDoc() {
+    const data = {
+      docId: this.docId,
+      from: 'document',
+    };
+    this.docService.getChatInDoc(data).subscribe(
+      (data: any) => {
+        this.chatDoc = data.getChatInDoc;
 
-	replyChat(item) {
-		this.replyFlag = true;
-		// console.log(item);
-		this.replyContent = item.chatContent;
-		this.replyMember = item.chatMember;
-		if (item.replyId) {
-			this.replyChatId = item.replyId;
-		} else {
-			this.replyChatId = item._id;
-		}
-	}
-	replyCancel() {
-		this.replyFlag = false;
-		this.replyContent = "";
-		this.replyMember = "";
-	}
+        for (let index = 0; index < this.chatDoc.length; index++) {
+          const date = this.commonService.dateFormatting(
+            this.chatDoc[index].createdAt,
+            'chatDate'
+          );
+          this.chatDoc[index].createdAt = date;
+        }
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
 
-	scrollToBottom(): void {
-		try {
-			this.scrolltop = this.myScrollContainer.nativeElement.scrollHeight;
-		} catch (err) {}
-	}
+  replyChat(item) {
+    this.replyFlag = true;
+    this.replyContent = item.chatContent;
+    this.replyMember = item.chatMember;
+    if (item.replyId) {
+      this.replyChatId = item.replyId;
+    } else {
+      this.replyChatId = item._id;
+    }
+  }
+  replyCancel() {
+    this.replyFlag = false;
+    this.replyContent = '';
+    this.replyMember = '';
+  }
 
-	// 사파리 fixed 문제
-	browserCheck() {
-		if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-			//모바일
-			this.mobileWidth = window.screen.width;
+  scrollToBottom(): void {
+    try {
+      this.scrolltop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
 
-			if (this.mobileWidth <= 780) {
-				// const maxHEditted_1 = document.querySelector<HTMLElement>('.maxHEditted_1');
+  // 사파리 fixed 문제
+  browserCheck() {
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      //모바일
+      this.mobileWidth = window.screen.width;
 
-				// 채팅부분 ////////////
-				document.getElementById("safari1").style.height = "calc(100vh - 330px)";
-				document.getElementById("safari2").style.position = "relative";
-				document.getElementById("safari2").style.bottom = "0px";
-				/////////////////////
-			}
-		}
-	}
+      if (this.mobileWidth <= 780) {
+        // const maxHEditted_1 = document.querySelector<HTMLElement>('.maxHEditted_1');
+
+        // 채팅부분 ////////////
+        document.getElementById('safari1').style.height = 'calc(100vh - 330px)';
+        document.getElementById('safari2').style.position = 'relative';
+        document.getElementById('safari2').style.bottom = '0px';
+        /////////////////////
+      }
+    }
+  }
 }
--- src/app/pages/spaces/document/doc-tab/doc-file-upload/file-upload-details/file-upload-details.component.ts
+++ src/app/pages/spaces/document/doc-tab/doc-file-upload/file-upload-details/file-upload-details.component.ts
@@ -1,21 +1,19 @@
-import { Component, Inject, OnInit } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { MaterialsModule } from "../../../../../../materials/materials.module";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MaterialsModule } from '../../../../../../materials/materials.module';
 
 @Component({
-	selector: "app-file-upload-details",
-	templateUrl: "./file-upload-details.component.html",
-	styleUrls: ["./file-upload-details.component.scss"],
-	standalone: true,
-	imports: [MaterialsModule],
+  selector: 'app-file-upload-details',
+  templateUrl: './file-upload-details.component.html',
+  styleUrls: ['./file-upload-details.component.scss'],
+  standalone: true,
+  imports: [MaterialsModule],
 })
 export class FileUploadDetailsComponent implements OnInit {
-	constructor(
-		public dialogRef: MatDialogRef<FileUploadDetailsComponent>,
-		@Inject(MAT_DIALOG_DATA) public data: any
-	) {}
+  constructor(
+    public dialogRef: MatDialogRef<FileUploadDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-	ngOnInit(): void {
-		console.log(this.data);
-	}
+  ngOnInit(): void {}
 }
