7feada335739d78a7a92061c05abd5ee22b7278e
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 11:03:54 +0900

    leaves requests 삭제

--- src/app/pages/employees/routes.ts
+++ src/app/pages/employees/routes.ts
@@ -1,5 +1,4 @@
 import { Route } from '@angular/router';
-import { LeavesRequestsComponent } from './leaves-requests/leaves-requests.component';
 import { LeavesStatusComponent } from './leaves-status/leaves-status.component';
 import { ManagersConnectionComponent } from './managers-connection/managers-connection.component';
 import { ReplacementDaysComponent } from './replacement-days/replacement-days.component';
@@ -8,10 +7,6 @@ export const EMPLOYEES_ROUTES: Route[] = [
   {
     path: 'leaves/status', // 직원들 휴가 사용 목록
     loadComponent: () => LeavesStatusComponent,
-  },
-  {
-    path: 'leaves/requests', // 직원들 휴가 요청 목록
-    loadComponent: () => LeavesRequestsComponent,
   },
   {
     path: 'leaves/replacement-requests', // 직원들 대체 휴일 요청 (휴일에 근무 시 대체휴가 요청 목록)
