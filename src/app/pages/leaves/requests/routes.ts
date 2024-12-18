3f2a4234c7791275e810bc55b8e1890d6f6a6332
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 11:18:07 +0900

    requests 삭제

--- src/app/pages/leaves/requests/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { RequestsComponent } from './requests.component';
-
-export const REQUESTS_ROUTES: Route[] = [
-  {
-    path: '', // 회사 공휴일 or 기념일
-    loadComponent: () => RequestsComponent,
-  },
-];
--- src/app/pages/leaves/routes.ts
+++ src/app/pages/leaves/routes.ts
@@ -10,14 +10,4 @@ export const LEAVES_ROUTES: Route[] = [
     path: 'my-status',
     loadComponent: () => StatusComponent,
   },
-  {
-    path: 'requests', // 회사 공휴일 or 기념일
-    loadChildren: () =>
-      import('./requests/routes').then((m) => m.REQUESTS_ROUTES),
-  },
-  {
-    path: 'requests', // 휴가 요청 목록
-    loadChildren: () =>
-      import('./requests/routes').then((m) => m.REQUESTS_ROUTES),
-  },
 ];
