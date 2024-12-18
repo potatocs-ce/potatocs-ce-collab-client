c4275062b10ad83950b3a24e49d59f17e9885e53
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 11:15:35 +0900

    replacement request 삭제

--- src/app/pages/leaves/replacement-requests/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { ReplacementRequestsComponent } from './replacement-requests.component';
-
-export const REPLACEMENT_REQUESTS_ROUTES: Route[] = [
-  {
-    path: '', // 회사 공휴일 or 기념일
-    loadComponent: () => ReplacementRequestsComponent,
-  },
-];
--- src/app/pages/leaves/routes.ts
+++ src/app/pages/leaves/routes.ts
@@ -1,27 +1,24 @@
-import { Route } from "@angular/router";
-import { LeavesRequestsAddComponent } from "./requests/leaves-requests-add/leaves-requests-add.component";
-import { StatusComponent } from "./status/status.component";
-import { LeavesComponent } from "./leaves.component";
-import { ReplacementRequestsComponent } from "./replacement-requests/replacement-requests.component";
+import { Route } from '@angular/router';
+import { LeavesRequestsAddComponent } from './requests/leaves-requests-add/leaves-requests-add.component';
+import { StatusComponent } from './status/status.component';
+import { LeavesComponent } from './leaves.component';
 
 export const LEAVES_ROUTES: Route[] = [
-	{
-		// 이정운 작업
-		// path: 'status', // 휴가 사용 현황
-		// loadComponent: () => StatusComponent
-		path: "my-status",
-		loadComponent: () => StatusComponent,
-	},
-	{
-		path: "requests", // 회사 공휴일 or 기념일
-		loadChildren: () => import("./requests/routes").then((m) => m.REQUESTS_ROUTES),
-	},
-	{
-		path: "requests", // 휴가 요청 목록
-		loadChildren: () => import("./requests/routes").then((m) => m.REQUESTS_ROUTES),
-	},
-	{
-		path: "rd-request-list",
-		loadComponent: () => ReplacementRequestsComponent,
-	},
+  {
+    // 이정운 작업
+    // path: 'status', // 휴가 사용 현황
+    // loadComponent: () => StatusComponent
+    path: 'my-status',
+    loadComponent: () => StatusComponent,
+  },
+  {
+    path: 'requests', // 회사 공휴일 or 기념일
+    loadChildren: () =>
+      import('./requests/routes').then((m) => m.REQUESTS_ROUTES),
+  },
+  {
+    path: 'requests', // 휴가 요청 목록
+    loadChildren: () =>
+      import('./requests/routes').then((m) => m.REQUESTS_ROUTES),
+  },
 ];
