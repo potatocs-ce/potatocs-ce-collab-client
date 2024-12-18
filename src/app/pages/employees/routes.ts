d9d58ac0dec47ed56efcc2c5920a0b12a1d0f97c
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-18 11:05:42 +0900

    managers connectoin 삭제

--- src/app/pages/employees/routes.ts
+++ src/app/pages/employees/routes.ts
@@ -1,15 +1,10 @@
 import { Route } from '@angular/router';
-import { ManagersConnectionComponent } from './managers-connection/managers-connection.component';
 import { ReplacementDaysComponent } from './replacement-days/replacement-days.component';
 
 export const EMPLOYEES_ROUTES: Route[] = [
   {
     path: 'leaves/replacement-requests', // 직원들 대체 휴일 요청 (휴일에 근무 시 대체휴가 요청 목록)
     loadComponent: () => ReplacementDaysComponent,
-  },
-  {
-    path: 'registration/requests', // 직원들이 담당 매니저와의 연결 요청 목록
-    loadComponent: () => ManagersConnectionComponent,
   },
   {
     path: '',
