ccbc8cbcbbef636879798a4435a3c83789474cc8
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-12-19 11:49:35 +0900

    face recognition 다이어로그 코드 정리리

--- src/app/components/dialogs/face-recognition-dialog/face-recognition-dialog.component.spec.ts
+++ src/app/components/dialogs/face-recognition-dialog/face-recognition-dialog.component.spec.ts
@@ -3,20 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FaceRecognitionDialogComponent } from './face-recognition-dialog.component';
 
 describe('FaceRecognitonDialogComponent', () => {
-  let component: FaceRecognitionDialogComponent;
-  let fixture: ComponentFixture<FaceRecognitionDialogComponent>;
+    let component: FaceRecognitionDialogComponent;
+    let fixture: ComponentFixture<FaceRecognitionDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FaceRecognitionDialogComponent],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FaceRecognitionDialogComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(FaceRecognitionDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(FaceRecognitionDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
