d42d259d0bbdef353fb15d67bb043a6f751d37b2
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-12-19 11:06:28 +0900

    guard spaceTime fix

--- src/app/guards/space.guard.ts
+++ src/app/guards/space.guard.ts
@@ -1,43 +1,49 @@
 import { inject } from '@angular/core';
 import {
-  Router,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  CanActivateFn,
+    Router,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    CanActivateFn,
 } from '@angular/router';
 import { DialogService } from '../stores/dialog/dialog.service';
 import { SideNavService } from '../stores/side-nav/side-nav.service';
 
 export const SpaceGuard: CanActivateFn = async (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
 ) => {
-  const dialogService = inject(DialogService);
-  const router = inject(Router);
-  const sidenavService = inject(SideNavService);
-  const spaces: any = await sidenavService.updateSideMenu().toPromise();
-  const spaceTime = route.params['spaceTime'];
-
-  let spaceInfo = spaces.navList[0].spaces;
-  let flag = false;
-  console.log('spaceTime : ', spaceTime);
-  for (let index = 0; index < spaceInfo.length; index++) {
-    const element = spaceInfo[index]._id;
-    if (spaceTime == element) {
-      flag = true;
-      break;
-    } else {
-      flag = false;
+    const dialogService = inject(DialogService);
+    const router = inject(Router);
+    const sidenavService = inject(SideNavService);
+    const spaces: any = await sidenavService.updateSideMenu().toPromise();
+    let spaceTime = ''
+    if (route.params['spaceTime']) {
+        spaceTime = route.params['spaceTime'];
     }
-  }
+    else {
+        spaceTime = route.queryParams['spaceTime']
+    }
 
-  if (flag) {
-    return true;
-  } else {
-    dialogService.openDialogNegative(
-      'You are not a member of this space or document.'
-    );
-    router.navigate(['/main']);
-  }
-  return false;
+    let spaceInfo = spaces.navList[0].spaces;
+    let flag = false;
+    console.log('spaceTime : ', spaceTime);
+    for (let index = 0; index < spaceInfo.length; index++) {
+        const element = spaceInfo[index]._id;
+        if (spaceTime == element) {
+            flag = true;
+            break;
+        } else {
+            flag = false;
+        }
+    }
+
+    if (flag) {
+        return true;
+    } else {
+        dialogService.openDialogNegative(
+            'You are not a member of this space or document.'
+        );
+        router.navigate(['/main']);
+    }
+    return false;
 };
